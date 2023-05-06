c2180e5b163fb8262704c22f99b2a03d5b0bd574
Patrick David <82005507+Patrick-David1@users.noreply.github.com>
2023-05-06 13:25:10 +0800

    Update Stable Fixes.cpp

--- Stable Fixes.cpp
+++ Stable Fixes.cpp
@@ -3,20 +3,13 @@
 #include <unistd.h>
 #include <math.h>
 
- using namespace std;
-
-
-//WRITTEN BY PATRICK DAVID// 
-//Bug Fixes//
+ using namespace std;
 
 int 
 main () 
-{
-    //Solving Quadratic Equations (b^ - 4(a,c))//
-    
-    //Holding our quadratic values//
+{
   int ax, bx, c;
-    //Other values//
+    
   int x;
 cout << "Enter standard form values." << "\n";
 
@@ -26,18 +19,17 @@ cout << "Enter the value of (b)\n";
 cin >> bx;
 cout << "Enter the value of (c)\n";
 cin >> c;
-  
- //The code will now perform squaring the value "b"//
+  
 cout << "Squaring the value (b)..." << "\n";
   
 int newbx;
 newbx = bx * bx;
 cout << "(b) is now " << newbx << "." << "\n";
-//FLAG - Equation update!//
+
 cout << "Our equation is now " << newbx << " - 4 (" << ax << "," << c << ")" << "\n";
-//Explanation//
+
 cout << "The code squared the given value of (b)\n" << "So b(" << bx << ") is (" << bx << " x " << bx << " = " << newbx << ")\n";
-//This code will multiply the values of the right side of the equation//
+
 cout << "Proceeding to solve the right side of the equation...\n";
   
 int optr;
@@ -49,15 +41,14 @@ int bsqoptr2;
 bsqoptr2 = bsqoptr * c;
 cout << "c(" << c << ") x " << bsqoptr << " is " << bsqoptr2 << ".\n";
   
-//Holds the answer discriminant//
+
 int ans;
 ans = newbx - bsqoptr2;
   
-//Getting the discriminant//
+
 cout << "The code subtracted both sides of the equation.\n" << "So the discriminant is " << newbx << " - " << bsqoptr2 << " = " << ans << ".\n";
 cout << "The discriminant is " << ans << ".\n";
- 
-//Identifying the nature of roots//
+ 
 int sqrtval;
 sqrtval = sqrt (ans);
   
